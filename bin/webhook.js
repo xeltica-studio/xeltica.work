76f217527dbc0322e74d5fb7c3b1029e594fa47a
Xeltica <citringo@gmail.com>
2018-01-22 02:36:44 +0900

    Add webhook

---
+++ bin/webhook.js
@@ -0,0 +1,37 @@
+"use strict"
+
+const conf = require("../lib/conf.js")
+const cmd = require("node-cmd")
+
+const PORT = process.env.PORT || 8080
+const SECRET = conf.webhook.secret //設定した任意のキー
+const REPOSITORY_NAME = conf.webhook.repo //リポジトリ名
+
+const http = require("http")
+const createHandler = require("github-webhook-handler")
+const handler = createHandler({
+  path: "/",
+  secret: SECRET
+})
+
+http.createServer((req, res) => {
+  handler(req, res, (err) => {
+    res.statusCode = 404
+    res.end("no such location")
+  })
+}).listen(PORT)
+
+
+handler.on("error", (err) => {
+  console.error("Error:", err.message)
+})
+
+
+handler.on("push", (event) => {
+  const payload = event.payload
+  const repoName = payload.repository.name
+  const branch = payload.ref.split("/").pop()
+  if (repoName === REPOSITORY_NAME && branch === "master") {
+    cmd.run('git pull && npm run rebuild');
+  }
+})
