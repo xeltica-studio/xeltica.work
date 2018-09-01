430c80ef1f8dd34b8967116a0082d1cb3086f54e
Xeltica <citringo@gmail.com>
2018-09-01 22:19:18 +0900

    キャラクター情報

--- src/ts/char.ts
+++ src/ts/char.ts
@@ -7,24 +7,15 @@ class Char {
 	citrine: ICharComponentSet;
 	kaho: ICharComponentSet;
 
+	readonly self = this;
+
 	constructor() {
 		this.getElements();
 		this.register();
-		switch (location.search.toLowerCase().substr(1)) {
-			default:
-			case "citrine":
-				this.updateView(this.citrine);
-				break;
-			case "kaho":
-				this.updateView(this.kaho);
-				break;
-		}
-		console.log();
+
+		this.syncView();
 	}
 
-	onPopState(e: PopStateEvent) {
-		 
-	}
 
 	getElements() {
 		this.citrine = {
@@ -38,20 +29,38 @@ class Char {
 	}
 
 	register() {
-		window.onpopstate = this.onPopState;
-		this.citrine.radioButton.onchange = e => {
-			this.updateView(this.citrine);
-		};
-		this.kaho.radioButton.onchange = e => {
-			this.updateView(this.kaho);
-		}
+		window.addEventListener("popstate", () => this.syncView());
+		this.citrine.radioButton.addEventListener("change", _ => {
+			this.setView(this.citrine);
+			history.pushState(null, null, "/char.html?citrine");
+
+		});
+		this.kaho.radioButton.addEventListener("change", _ => {
+			this.setView(this.kaho);
+			history.pushState(null, null, "/char.html?kaho");
+		});
 	}
 
-	updateView(char: ICharComponentSet) {
-		if (char != this.citrine) this.citrine.view.classList.remove("show");
-		if (char != this.kaho) this.kaho.view.classList.remove("show");
+	private setView(char: ICharComponentSet) {
+		if (!char) return;
+
+		if (char !== this.citrine) this.citrine.view.classList.remove("show");
+		else if (char !== this.kaho) this.kaho.view.classList.remove("show");
 		
 		char.view.classList.add("show");
+	}
+
+	private syncView() {
+		switch (location.search.toLowerCase().substr(1)) {
+			default:
+			case "citrine":
+				this.setView(this.citrine);
+				break;
+			case "kaho":
+				this.setView(this.kaho);
+				break;
+		}
+		console.log(location.search);
 	}
 
 }
